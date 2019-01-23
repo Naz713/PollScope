612d6d22b12a4516a16ac2d7a79cbb7301fbfb63
Naz713 <naz7771313@gmail.com>
2019-01-22 18:48:26 -0600

    llamada a actualizar timelapse

--- app/src/main/java/com/nazdesigns/polascope/EditActivity.java
+++ app/src/main/java/com/nazdesigns/polascope/EditActivity.java
@@ -3,6 +3,8 @@ package com.nazdesigns.polascope;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.nazdesigns.polascope.GameStructure.TimeLapse;
@@ -12,22 +14,41 @@ public class EditActivity extends Activity {
     private TimeLapse mTL;
     private EditText mResume;
     private EditText mLongText;
+    private Button mDescarta;
+    private Button mGuarda;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mIndex = getIntent().getIntArrayExtra("index");
+
         setContentView(R.layout.activity_edit);
+
         mResume = findViewById(R.id.resume);
         mLongText = findViewById(R.id.long_text);
+
+        mDescarta = findViewById(R.id.buton_descarta);
+        mDescarta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mResume.setText(mTL.getResume());
+                mLongText.setText(mTL.getBody());
+            }
+        });
+
+        mGuarda = findViewById(R.id.buton_guarda);
+        mGuarda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTL.setResume(mResume.getText().toString());
+                mTL.setBody(mLongText.getText().toString());
+                ( (PolApp) getApplication()).setBranch(mIndex, mTL);
+            }
+        });
 
         mTL = ((PolApp) getApplication()).getBranch(mIndex);
 
         mResume.setText(mTL.getResume());
         mLongText.setText(mTL.getBody());
     }
-
-    // TODO: Llamar de vuelta a PolApp para actualizar la informacion
-    // cuando se actualice la informacion
-
  }
